23671a0b1e5a170c04dc09e87439e074f3d0ccfe
Conan Bitter <conan.bitter@gmail.com>
2024-10-08 18:54:00 +0300

    Implement ordered dithering

--- imgconv/src/color.rs
+++ imgconv/src/color.rs
@@ -1,5 +1,5 @@
 fn convert8to5(d: u8) -> u8 {
-    let result = (d as f64 * 0b11111 as f64 / 0b11111111 as f64) as u8;
+    let result = (d as f64 * 0b11111 as f64 / 0b11111111 as f64).round() as u8;
     if result > 0b11111 {
         0b11111
     } else {
@@ -8,7 +8,7 @@ fn convert8to5(d: u8) -> u8 {
 }
 
 fn convert8to6(d: u8) -> u8 {
-    let result = (d as f64 * 0b111111 as f64 / 0b11111111 as f64) as u8;
+    let result = (d as f64 * 0b111111 as f64 / 0b11111111 as f64).round() as u8;
     if result > 0b111111 {
         0b111111
     } else {
@@ -17,7 +17,7 @@ fn convert8to6(d: u8) -> u8 {
 }
 
 fn convert5to8(d: u8) -> u8 {
-    let result = (d as f64 * 0b11111111 as f64 / 0b11111 as f64) as u16;
+    let result = (d as f64 * 0b11111111 as f64 / 0b11111 as f64).round() as u16;
     if result > 0b11111111 {
         0b11111111
     } else {
@@ -26,7 +26,7 @@ fn convert5to8(d: u8) -> u8 {
 }
 
 fn convert6to8(d: u8) -> u8 {
-    let result = (d as f64 * 0b11111111 as f64 / 0b111111 as f64) as u16;
+    let result = (d as f64 * 0b11111111 as f64 / 0b111111 as f64).round() as u16;
     if result > 0b11111111 {
         0b11111111
     } else {
@@ -42,10 +42,10 @@ pub fn find_color(r: u8, g: u8, b: u8) -> u16 {
 }
 
 pub fn find_color_float(r: f64, g: f64, b: f64) -> u16 {
-    let r = ((r * 0b111111 as f64) as u16).max(0b11111);
-    let g = ((g * 0b1111111 as f64) as u16).max(0b111111);
-    let b = ((b * 0b111111 as f64) as u16).max(0b11111);
-    (r as u16) << 11 | (g as u16) << 5 | (b as u16)
+    let r = ((r.clamp(0.0, 1.0) * 0b11111 as f64).round() as u16).min(0b11111);
+    let g = ((g.clamp(0.0, 1.0) * 0b111111 as f64).round() as u16).min(0b111111);
+    let b = ((b.clamp(0.0, 1.0) * 0b11111 as f64).round() as u16).min(0b11111);
+    r << 11 | g << 5 | b
 }
 
 pub fn revert_color(color: u16) -> (u8, u8, u8) {
--- imgconv/src/main.rs
+++ imgconv/src/main.rs
@@ -1,19 +1,19 @@
 use anyhow::Result;
 use image::ImageReader;
 
+mod color;
+mod dithering;
 mod image_raw;
 
 fn main() -> Result<()> {
     let img = ImageReader::open("../test_data/test.png")?.decode()?.to_rgb8();
 
     let mut raw = image_raw::Image::new(img.width(), img.height());
+    let converter = dithering::get_converter("pat8");
 
     for (x, y, c) in img.enumerate_pixels() {
-        let r = c[0] >> 3;
-        let g = c[1] >> 2;
-        let b = c[2] >> 3;
         let index = x + y * raw.width;
-        let color_converted: u16 = (r as u16) << 11 | (g as u16) << 5 | (b as u16);
+        let color_converted = converter(x, y, c[0], c[1], c[2]);
         raw.data[index as usize] = color_converted;
     }
 
